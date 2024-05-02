63f8dd02c7d7c1470422b26c859568ccb1c48214
npoctonomorute <makarafanasev07@gmail.com>
2024-05-02 23:49:17 +0300

    experiment with json

--- src/JSON/JsonParser.java
+++ src/JSON/JsonParser.java
@@ -7,26 +7,25 @@ import com.google.gson.reflect.TypeToken;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.StringReader;
+import java.io.FileReader;
 import java.util.Date;
 import java.util.List;
 
 public class JsonParser {
     private final String fileName;
-    private final String test = "";
     public JsonParser(String fileName) {
         this.fileName = fileName;
     }
     public CollectionManager parse() throws FileNotFoundException {
         Gson gson = new Gson();
-        //BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        BufferedReader reader = new BufferedReader(new StringReader(test));
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
         List<Worker> workers = gson.fromJson(reader, new TypeToken<List<Worker>>(){}.getType());
         CollectionManager collection = new CollectionManager();
 
         for (Worker worker : workers) {
             worker.setId(CollectionManager.generateId());
             worker.setCreationDate(new Date());
+            collection.add(worker);
         }
         return collection;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 import App.AppContainer;
+import App.CollectionManager;
 import App.Invoker;
+import JSON.JsonParser;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        JsonParser parser = new JsonParser(args[0]);
+        CollectionManager collection = parser.parse();
+        System.out.println(collection.getMap());
         Scanner sc = new Scanner(System.in);
         AppContainer.setSc(sc);
         Invoker invoker = new Invoker();
@@ -14,4 +20,3 @@ public class Main {
         }
     }
 }
-
