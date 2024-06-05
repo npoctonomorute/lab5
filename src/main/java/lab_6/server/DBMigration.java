1fb95ea7739137b5acd293ca26b62b56ab8ad027
web.witcher <makarafanasev07@gmail.com>
2024-06-05 17:07:03 +0300

    Create DBMigration.java

---
+++ src/main/java/lab_6/server/DBMigration.java
@@ -0,0 +1,47 @@
+package lab_6.server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBMigration {
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/makar";
+    public static final String DB_USER = "postgres";
+    public static final String DB_PASSWORD = "root";
+
+    public static void main(String[] args) throws SQLException {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            connection.createStatement().execute("""
+                    CREATE TABLE IF NOT EXISTS workers
+                    (
+                        id            bigserial,
+                        name          varchar(255)     NOT NULL,
+                        creation_date date             NOT NULL DEFAULT now(),
+                        salary        double precision NOT NULL,
+                        start_date    date             NOT NULL,
+                        "position"    smallint,
+                        status        smallint         NOT NULL,
+                        p_birthday    date,
+                        p_hair_color  smallint,
+                        p_nationality smallint,
+                        p_loc_x       double precision,
+                        p_loc_y       double precision,
+                        p_loc_z       double precision,
+                        p_loc_name    varchar(255),
+                        owner_login   varchar(255)     NOT NULL,
+                        CONSTRAINT workers_pkey PRIMARY KEY (id)
+                    )
+                    """);
+            connection.createStatement().execute("""
+                    CREATE TABLE IF NOT EXISTS users
+                    (
+                        id       bigserial,
+                        login    varchar(255) NOT NULL,
+                        password varchar(255) NOT NULL,
+                        CONSTRAINT users_pkey PRIMARY KEY (id),
+                        CONSTRAINT login_unique UNIQUE (login)
+                    )
+                    """);
+        }
+    }
+}
